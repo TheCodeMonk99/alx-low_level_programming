42f2706084dfd8fbebef34e405bc4b8dffcb12e7
TheCodeMonk <rugutkclinton@gmail.com>
2023-06-15 10:24:40 +0000

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
-  *main - Entry point
-  *
-  *return - Always 0
-  */
+   * main - Entry point
+    *
+     * Return: Always 0 (Success)
+      */
 int main()
 {
-	char words = "Programming is like building a multilingual puzzle";
-	puts (words);
-	return 0;
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
 }
