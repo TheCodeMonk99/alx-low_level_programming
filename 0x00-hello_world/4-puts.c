00a7ccef05c127710430464623e2b5d22255d1df
TheCodeMonk <rugutkclinton@gmail.com>
2023-06-15 10:06:43 +0000

    This is a happy place

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+  *main - Entry point
+  *
+  *return - Always 0
+  */
+int main()
+{
+	char words = "Programming is like building a multilingual puzzle";
+	puts (words);
+	return 0;
+}
