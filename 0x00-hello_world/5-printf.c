0ec23eca187e9fb1922b6746a704b351fea2e66b
TheCodeMonk <rugutkclinton@gmail.com>
2023-06-15 10:36:05 +0000

    Changes

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,8 +2,8 @@
 /**
   *main -Entry point
   *
-  *return - always (0) Sucess
-  */
+  *Return: Always 0 (Sucess)
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
