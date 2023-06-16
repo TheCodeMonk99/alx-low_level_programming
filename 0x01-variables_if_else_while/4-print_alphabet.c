17c8131663ce9c0af834b6d39d2f88987de9f6ec
TheCodeMonk <rugutkclinton@gmail.com>
2023-06-16 09:23:06 +0000

    Push

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+  *main - entry point
+  *print alphabet characters except e and q
+  *Return: always 0 (success)
+  */
+
+int main(void)
+{
+	int ch = 'a';
+	while (ch <= 'z')
+	{
+		if (ch != 'e' && ch != 'q')
+			putchar(ch);
+		ch++
+	}
+	putchar(10);
+	return (0);
+}
