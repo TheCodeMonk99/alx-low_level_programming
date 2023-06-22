b754b35603fceb82152336e844e7664abea418be
TheCodeMonk <rugutkclinton@gmail.com>
2023-06-22 09:48:10 +0000

    First task

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+/**
+  *main - find out if char is uppercase
+  * @c: character to be checked
+  *
+  * Return: 1 if c is uppercase
+  * 0 otherwise
+  */
+
+int _isupper(int c)
+{
+	int c;
+
+	if (c <= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This will solve te tasks located in More functions and nested loops
