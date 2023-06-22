b0a1b57b34c7c58c1c5212f63fd73c7151a9c98c
TheCodeMonk <rugutkclinton@gmail.com>
2023-06-22 14:50:22 +0000

    The message

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  * mul - multiplies 2 digits
+  * @a: first digit
+  * @b: second digit
+  * Return: product of the 2 digits
+  */
+
+int mul(int a, int b)
+{
+	int a, b, res;
+
+	res = (a * b);
+	return (res);
+}
