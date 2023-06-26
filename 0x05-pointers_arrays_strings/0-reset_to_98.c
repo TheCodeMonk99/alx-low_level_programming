5e1593d253d677ee57d3c66725f19867bf75a875
TheCodeMonk <rugutkclinton@gmail.com>
2023-06-26 08:06:58 +0000

    What a lively Monday

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+/**
+  * reset_to_98 - sets the value to 98
+  * @ *n: pointer variable
+  *Return: Always 0
+  */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+This directory contains code that will solve projects in the ALX
+programming tasks
+
