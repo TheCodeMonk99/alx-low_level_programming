9cb7d4c37a32c364d5405052191db83f8d7a95cb
TheCodeMonk99 <rugutkclinton@gmail.com>
2023-06-29 13:16:05 +0000

    Testing 1,2,3

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+/**
+  * _strcat - concatanates two strings
+  * @dest: first string
+  * @src: string to be appended to dest
+  *
+  * Return: Appended string dest
+  */
+
+char *_strcat(char *dest, char *src);
+{
+	int i, m;
+
+	i = 0;
+	while (*dest != '\0')
+	{
+		i++;
+		dest++;
+	}
+	i++;
+	for (m = 0; src[m] != '\0'; m++)
+	{
+		dest[i] = src[m];
+		i++;
+	}
+	dest[i + 1] = '\0';
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,5 @@
+This directory contains code that solves tasks on ALX platform
+1. A function that concatenates two strings
+2. A function that concatenates two string to n bytes
+ 3 A function that copies a string
+ 
