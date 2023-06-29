644c81483ec177bfdae614de86ba808a817da07c
TheCodeMonk99 <rugutkclinton@gmail.com>
2023-06-29 13:55:17 +0000

    The second test

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+  * _strncat - concatanated 2 strings to n bytes
+  * @dest: first string
+  * @src: string to append
+  * @n: number of bytes
+  *
+  * Return: Appended string dest
+  */
+
+char *_strncat(char *dest, char *src, int n);
+{
+	int i, m;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	for (m = 0; m < n && m != '\0'; m++)
+	{
+		dest[i] = src[m];
+		m++;
+		i++;
+	}
+	if (m < n)
+		dest[i] = '\0';
+	return (dest);
+}
