bf54ad77ad0708f0f818b9ab25dd1d9f9d8b643d
TheCodeMonk99 <rugutkclinton@gmail.com>
2023-07-05 10:32:01 +0000

    Trust

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
-  * _puts_recusrion - prints string
+  * _puts_recursion - prints characters in string followed by new line
   * @s: pointer variable
-  * Return: void
+  * Return: Alwasys 0 success
   */
 void _puts_recursion(char *s)
 {
 	if (*s)
 	{
 		_putchar(*s);
-		_puts_recursion(s++);
+		_puts_recursion(s + 1);
 	}
 	else
 	{ _putchar(10);
