005ee7adf3188e262cff56bc9ecaab68d4bbf622
TheCodeMonk99 <rugutkclinton@gmail.com>
2023-07-07 13:41:35 +0000

    second task

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * main - prints number of arguments passed into it
+  * @argc: argument count
+  * @argv: argument vector
+  * Return: Always 0 (Sucess)
+  */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
