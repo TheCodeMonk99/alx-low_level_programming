c3d029bcda05af2f0d2629afe62aa74c82e978e3
TheCodeMonk99 <rugutkclinton@gmail.com>
2023-07-07 13:33:22 +0000

    Vectors and counts

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,10 +7,9 @@
   * @argv: argument vector
   * Return: Always 0 success
   */
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc __attribute__((unused)), char *argv)
 {
-	int i = 0;
+	printf("%s\n", *argv);
 
-	printf("%s\n", argv[0]);
 	return (0);
 }
