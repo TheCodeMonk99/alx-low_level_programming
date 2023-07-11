0e0af98a7f17bfbc037e6afa558c1ff4e45972f0
TheCodeMonk99 <rugutkclinton@gmail.com>
2023-07-11 19:27:27 +0000

    Push

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,9 +9,9 @@
 char *create_array(unsigned int size, char c)
 {
 	char *p;
-	int i;
+	unsigned int i;
 
-	p = malloc(size of(char) * size);
+	p = malloc(sizeof(char) * size);
 	if (size == 0 || p == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
