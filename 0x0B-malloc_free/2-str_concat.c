0358ca0b55a811b8ae493b0f4fe52af6a32a5342
TheCodeMonk99 <rugutkclinton@gmail.com>
2023-07-11 20:10:23 +0000

    Push

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,33 @@
+#include <stdlib.>
+#include "main.h"
+
+/**
+  * str_concat - concatanates 2 s1 & s2
+  * @s1: first string
+  * @s2: second string
+  * Return: poninter to new string
+  */
+char *str_concat(char *s1, char *s2)
+{
+	unsigned int a, b, c, i;
+	char *p;
+
+	a = b = c = i = 0;
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		se = "";
+	while (s1[a] != '\0')
+		a++;
+	while (s2[b] != '\0')
+		b++;
+	p = malloc(sizeof(char) * (a + b + 1);
+	if (p == NULL)
+		return (NULL);
+	for (; c < a; c++)
+		p[c] = s1[c];
+	for (i = 0; c < (a + b); c++)
+		p[c] = s2[i];
+	p[c] = '\0'
+	return (p);
+}
