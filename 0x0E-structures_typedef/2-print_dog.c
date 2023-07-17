c7410a77fbf4a1290b0f788c03479c6026335355
TheCodeMonk99 <rugutkclinton@gmail.com>
2023-07-17 15:30:13 +0000

    Serial

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,10 +10,10 @@
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
-		return (1);
+		return;
 	if (d->name == NULL)
 		d->name = "(nil)";
 	if (d->owner == NULL)
 		d->owner = "(nil)";
-	printf("Name: %s\nAge: %d\nOwner: %s\n", d->name, d->age, d->owner);
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
