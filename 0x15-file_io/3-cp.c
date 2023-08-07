e00b6eb291574ee5cfbd81e35e73599bcb6bf826
TheCodeMonk99 <rugutkclinton@gmail.com>
2023-08-07 15:08:04 +0000

    Copy cat

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,105 @@
+#include "main.h"
+#include <stdio.h>
+
+/**declaring functions*/
+void close_fd(int fd); /*closes fd*/
+char *create_buffer(void); /*creates buffer*/
+/**
+  * close_fd - prints error when cannot close fd
+  * @fd: fd unable to close
+  * Return: nothing
+  */
+void close_fd(int fd)
+{
+	int a = close(fd);
+
+	if (a == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+/**
+  * main - copies from one file to another
+  * @argc: argument count
+  * @argv: arguments
+  * Return: Always 0
+  * Description:
+  * exit code 97 - argument count incorrect
+  * exit code 98 - file_from doesn't exist or cannot be read
+  * exit code 99 - file_to cannot be created or writted to
+  * exit code 100 - file cannot be closed
+  */
+int main(int argc, char *argv[])
+{
+	int fd1, fd2, red, wr;
+
+	char *temp_s;
+
+	/*checking no. of arguments*/
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	/*open file from*/
+	fd1 = open(argv[1], O_RDONLY);
+	if (fd1 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
+	/*open file to*/
+	fd2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0774);
+
+	/*write to buffer*/
+	temp_s = create_buffer();
+	red = read(fd1, temp_s, 1024);
+
+	for (; red > 0;)
+	{
+		if (fd1 == -1 || red == -1)
+		{	dprintf(STDERR_FILENO,
+			"Error: Can't read from file %s\n", argv[1]);
+			free(temp_s);
+			exit(98);
+		}
+
+		wr = write(fd2, temp_s, red);
+		if (fd2 == -1 || wr == -1)
+		{
+			dprintf(STDERR_FILENO,
+			"Error: Can't write to %s\n", argv[2]);
+			free(temp_s);
+			exit(99);
+		}
+		red = read(fd1, temp_s, 1024);
+		fd2 = open(argv[2], O_WRONLY | O_APPEND);
+	}
+
+	free(temp_s);
+	close_fd(fd1);
+	close_fd(fd2);
+	return (0);
+}
+
+/**
+  * create_buffer - creates buffer and allocates 1024 bytes
+  *
+  * Return: returns buffer
+  */
+char *create_buffer(void)
+{
+	char *store;
+
+	store = malloc(sizeof(char) * 1024);
+
+	if (store == NULL)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to\n");
+		exit(99);
+	}
+	return (store);
+}
