64df1c00c712e6269f7bfbcd739cbe0181b2210c
TheCodeMonk99 <rugutkclinton@gmail.com>
2023-08-07 12:41:31 +0000

    Add fcntl.h in main.h

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 			i++;
 	}
 
-	fd = open(filename, O_CREATE | O_RDWR | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	/*writing into the file*/
 	wr = write(fd, text_content, i);
 	if (fd == -1 || wr == -1)
